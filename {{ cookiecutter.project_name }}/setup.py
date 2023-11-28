251082d1b7a081bf46e9518378113bd4907de979
agentlans <agentlans@users.noreply.github.com>
2023-11-27 22:38:18 -0500

    Add files via upload

---
+++ {{ cookiecutter.project_name }}/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='{{ cookiecutter.project_name }}',
+      version='0.0.1',
+      description='{{ cookiecutter.description }}',
+      author='{{ cookiecutter.author }}',
+      packages=['{{ cookiecutter.package_name }}'],
+      license='{{ cookiecutter.license }}',
+      )
